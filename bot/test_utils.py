c0761e805fd56933a3255fbdac874d9695051959
rsperoni <rsperoni@mgcoders.com>
2017-04-12 15:30:13 -0300

    utils file

---
+++ bot/test_utils.py
@@ -0,0 +1,48 @@
+import unittest
+from utils import utf8,escapeMarkdown,checkAndFixUrl
+
+class UtilsTestCase(unittest.TestCase):
+    """Tests for `utils.py`."""
+
+    def test_solo_hostname(self):
+        """Solo hostname"""
+        self.assertEqual('http://prueba.com',checkAndFixUrl('prueba.com'))
+
+    def test_hostname_puerto(self):
+        """Hostname y puerto"""
+        self.assertEqual('http://prueba.com:8181',checkAndFixUrl('prueba.com:8181'))
+
+    def test_hostname_puerto_www(self):
+        """Hostname, www y puerto"""
+        self.assertEqual('http://www.prueba.com:8181',checkAndFixUrl('www.prueba.com:8181'))
+
+    def test_solo_hostname_www(self):
+        """Hostname y www"""
+        self.assertEqual('http://www.prueba.com',checkAndFixUrl('www.prueba.com'))
+
+    def test_hostname_puerto_www_path(self):
+        """Hostname, www, puerto y path"""
+        self.assertEqual('http://www.prueba.com:8181/youtrack',checkAndFixUrl('www.prueba.com:8181/youtrack'))
+
+    def test_hostname_puerto_path(self):
+        """Hostname, puerto y path"""
+        self.assertEqual('http://prueba.com:8181/youtrack',checkAndFixUrl('prueba.com:8181/youtrack'))
+
+    def test_hostname_path(self):
+        """Hostname y path"""
+        self.assertEqual('http://prueba.com/youtrack',checkAndFixUrl('prueba.com/youtrack'))
+
+    def test_ip(self):
+        """Ip"""
+        self.assertEqual('http://127.0.0.1',checkAndFixUrl('127.0.0.1'))
+
+    def test_ip_puerto(self):
+        """Ip y puerto"""
+        self.assertEqual('http://127.0.0.1:8989',checkAndFixUrl('127.0.0.1:8989'))
+
+    def test_ip_puerto_path(self):
+        """Ip, path y puerto"""
+        self.assertEqual('http://127.0.0.1:8989/youtrack',checkAndFixUrl('127.0.0.1:8989/youtrack'))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ bot/utils.py
@@ -0,0 +1,26 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+import socket
+import urlparse
+
+def utf8(unicode_text):
+    return unicode_text.encode('utf-8')
+
+def checkAndFixUrl(raw_data):
+    finalUrl = None
+    url = urlparse.urlparse(raw_data)
+    if not url.scheme or not url.scheme.startswith('http'):
+        raw_data = 'http://'+raw_data
+        url = urlparse.urlparse(raw_data)
+    #Ver si resuelve o excepcion
+    hostname = socket.gethostbyname(url.hostname)
+    if url.path:
+        finalUrl = url.scheme+'://'+url.netloc+url.path
+    else:
+        finalUrl = url.scheme+'://'+url.netloc
+    return finalUrl
+
+def escapeMarkdown(text):
+    text = text.replace('_','\_')
+    text = text.replace('*','\*')
+    return text
