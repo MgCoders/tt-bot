111695d5dca241042db11cc3c7d3b401bc3aa34e
rsperoni <rsperoni@mgcoders.com>
2017-04-20 11:50:59 +0000

    :smile:

--- check_service.sh
+++ check_service.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 set -x
-docker-compose --file=docker-compose-development.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do  
+docker-compose --file=docker-compose.produccion.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do  
     if [ "$code" == "1" ]; then    
        exit -1
     fi
--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,4 @@
 echo cd tt-bot-deploy en home
 cd /home/ubuntu/tt-bot-deploy
 echo docker-compose up
-docker-compose -f docker-compose.produccion.yml up -d
+docker-compose -f docker-compose.produccion.yml up -d --build bot
