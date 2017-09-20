d1536c91c1ecaf786c6a91c88f3b5a53c924c56d
rsperoni <rsperoni@mgcoders.com>
2017-09-19 22:15:28 -0300

    cambios deploy 6

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,6 @@
 echo cd tt-bot-deploy en home
 cd /home/ubuntu/tt-bot-deploy
 echo docker-compose up
+$(aws ecr get-login --region us-east-1)
+docker-compose -f docker-compose.produccion.yml pull --build
 docker-compose -f docker-compose.produccion.yml up -d --build
