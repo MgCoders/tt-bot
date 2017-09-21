f44a04ba88f32f514af3db9bee375df15890a2b3
rsperoni <rsperoni@mgcoders.com>
2017-09-21 17:23:52 -0300

    enesima prueba 2

--- start_server.sh
+++ start_server.sh
@@ -2,6 +2,13 @@
 
 echo cd tt-bot-deploy en home
 cd /home/ubuntu/tt-bot-deploy
-echo docker-compose up
 $(aws ecr get-login --region us-east-1)
 docker-compose -f docker-compose.produccion.yml pull bot && docker-compose -f docker-compose.produccion.yml up -d --build bot
+if [ $? -eq 0 ]
+then
+  echo "Successfull"
+  exit 0
+else
+  echo "Error" >&2
+  exit 1
+fi
