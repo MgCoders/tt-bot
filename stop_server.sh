2d1f4b4493caff895eeac5ef5fd3797a770dd089
rsperoni <rsperoni@mgcoders.com>
2017-09-21 17:25:00 -0300

    enesima prueba 2

--- stop_server.sh
+++ stop_server.sh
@@ -3,5 +3,12 @@ set -x
 
 echo cd tt-bot-deploy en home
 cd /home/ubuntu/tt-bot-deploy
-echo docker-compose kill
-docker-compose -f docker-compose.produccion.yml kill
+docker-compose -f docker-compose.produccion.yml kill && docker-compose -f docker-compose.produccion.yml rm -f
+if [ $? -eq 0 ]
+then
+  echo "Successfull"
+  exit 0
+else
+  echo "Error" >&2
+  exit 1
+fi
